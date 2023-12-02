751ab51d4bd202ba34490b604f7944c6070b7b01
Iain <iain.coffield@gmail.com>
2023-12-01 19:46:49 -0600

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,54 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .map { it.replace("""\D""".toRegex(), "") }
+            .map { it.first().toString() + it.last().toString() }
+            .sumOf { it.toInt() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .map { it.toStringDigits() }
+            .map { it.first().toString() + it.last().toString() }
+            .sumOf { it.toInt() }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    println("Part One")
+    val testInput = readInput("Day01_p1_test")
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
+
+    println("Part Two")
+    val testInputP2 = readInput("Day01_p2_test")
+    check(part2(testInputP2) == 281)
+
     part2(input).println()
+}
+
+private fun String.toStringDigits(): String {
+    return this
+        .windowed(this.length, partialWindows = true)
+        .joinToString("") { it.toStringDigit() }
+}
+
+private fun String.toStringDigit(): String {
+    if (this.first().isDigit()) {
+        return this.first().toString()
+    }
+
+    return when {
+        this.startsWith("one") -> "1"
+        this.startsWith("two") -> "2"
+        this.startsWith("three") -> "3"
+        this.startsWith("four") -> "4"
+        this.startsWith("five") -> "5"
+        this.startsWith("six") -> "6"
+        this.startsWith("seven") -> "7"
+        this.startsWith("eight") -> "8"
+        this.startsWith("nine") -> "9"
+        else -> ""
+    }
 }
